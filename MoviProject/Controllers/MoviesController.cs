6dbe5276d83377c90ddd93ce126ef0aa45b25040
Liza <Lizadobrogost@outlook.com>
2020-09-02 01:19:12 +0300

    More changes

--- MoviProject/Controllers/MoviesController.cs
+++ MoviProject/Controllers/MoviesController.cs
@@ -16,6 +16,7 @@ namespace MoviProject.Controllers
 
             return View(movies);
         }
+
         private IEnumerable<Movie> GetMovies()
         {
             return new List<Movie>
@@ -24,39 +25,24 @@ namespace MoviProject.Controllers
                 new Movie { Id = 2, Name = "Wall-e" }
             };
         }
-        // GET: Movies
-        public ActionResult Random()
-        {
-            var movie = new Movie() {Name = "Shrek!"};
-            var viewResul=new  ViewResult();
-            var customer=new List<Customer>()
-            {
-                new Customer(){Name = "Customer 1"},
-                new Customer(){Name ="Customer 2"}
-            };
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customer
-            };
-            return View(viewModel);
-        }
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        //public ActionResult ByReleaseDate(int year,int month)
-        //{
-        //    return Content(year+"/"+month);
-        //}
 
-        //public ActionResult Index(int? pageIndex, string sortBy)
+        // GET: Movies/Random
+        //public ActionResult Random()
         //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
         //    {
-        //        sortBy = "Name";
-        //    }
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2" }
+        //    };
 
-        //    return Content(String.Format("pageIndex={0}&sortby{1}", pageIndex, sortBy));
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
         //}
     }
 }
