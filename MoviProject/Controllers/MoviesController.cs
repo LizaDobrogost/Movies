d326313f2ef4af1dfce26ab67ce96f07d658c28e
Liza <Lizadobrogost@outlook.com>
2020-09-01 23:31:11 +0300

    MoviesController changed

--- MoviProject/Controllers/MoviesController.cs
+++ MoviProject/Controllers/MoviesController.cs
@@ -10,6 +10,20 @@ namespace MoviProject.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies
         public ActionResult Random()
         {
@@ -17,8 +31,8 @@ namespace MoviProject.Controllers
             var viewResul=new  ViewResult();
             var customer=new List<Customer>()
             {
-                new Customer(){Name = "John Smith"},
-                new Customer(){Name ="Mary Wiliams"}
+                new Customer(){Name = "Customer 1"},
+                new Customer(){Name ="Customer 2"}
             };
             var viewModel = new RandomMovieViewModel()
             {
@@ -27,22 +41,22 @@ namespace MoviProject.Controllers
             };
             return View(viewModel);
         }
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month)
-        {
-            return Content(year+"/"+month);
-        }
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year,int month)
+        //{
+        //    return Content(year+"/"+month);
+        //}
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
 
-            return Content(String.Format("pageIndex={0}&sortby{1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortby{1}", pageIndex, sortBy));
+        //}
     }
 }
